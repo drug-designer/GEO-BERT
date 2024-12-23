e81f2d0dd842f4d9ecf2b6d0af15f65825831652
drug-designer <tutzhangxiang@163.com>
2024-12-23 15:35:17 +0800

    Update conformer_select.py

--- conformer_select.py
+++ conformer_select.py
@@ -1,8 +1,6 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
-
-
 def has_3D_conformer(molecule):
     """Check if a molecule has a 3D conformer."""
     AllChem.EmbedMolecule(molecule)
@@ -13,13 +11,9 @@ def filter_molecules(input_file, output_file):
     with open(input_file, 'r') as infile:
         lines = infile.readlines()
 
-    # 提取SMILES列名
     header = lines[0].strip()
 
-    # 将SMILES列名和分子SMILES分开
     smiles_list = [line.strip() for line in lines[1:] if line.strip()]
-
-
 
     # Convert SMILES to RDKit molecules
     molecules=[]
@@ -28,9 +22,7 @@ def filter_molecules(input_file, output_file):
        a=Chem.AddHs(Chem.MolFromSmiles(smiles))
        molecules.append(a)
 
-
     # Filter molecules with 3D conformers
-
     molecules_with_3D = []
     i=0
     for smiles in molecules:
@@ -39,7 +31,6 @@ def filter_molecules(input_file, output_file):
         print(Chem.MolToSmiles(Chem.RemoveHs(smiles)))
         if has_3D_conformer(smiles):
           molecules_with_3D.append(smiles)
-
 
     # Convert molecules back to SMILES
     filtered_smiles_list = [Chem.MolToSmiles(Chem.RemoveHs(mol)) for mol in molecules_with_3D]
@@ -51,9 +42,7 @@ def filter_molecules(input_file, output_file):
         # Write the filtered SMILES
         outfile.write('\n'.join(filtered_smiles_list))
 
-# 输入和输出文件的路径
-input_filepath = 'data/chembl_50wan_3.txt'
-output_filepath = 'data/chembl_conformer_select_50wan_3.txt'
+input_filepath = 'data/chembl_145wan.txt'
+output_filepath = 'data/chembl_conformer_select_145wan.txt'
 
-# 执行筛选操作
 filter_molecules(input_filepath, output_filepath)
