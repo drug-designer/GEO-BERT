3cd7a7054749660b968a4a775ee043d435d8681c
drug-designer <tutzhangxiang@163.com>
2024-12-23 18:13:28 +0800

    Update Class_hyperopt_DYRK1A.py

--- Class_hyperopt_DYRK1A.py
+++ Class_hyperopt_DYRK1A.py
@@ -44,9 +44,7 @@ def count_parameters(model):
 
 def main(seed, args):
 
-    # tasks = ['BBBP', 'bace', 'HIV','clintox', 'tox21', 'muv', 'sider','toxcast_data']
-
-    task = 'BBBP'
+    task = 'clintox'
 
     elif task =='BBBP':
         label = ['p_np']
